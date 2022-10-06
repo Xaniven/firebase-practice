217cff936adee4783d5d6145913f20b637ed08ef
Xaniven <k.kelley5978@gmail.com>
2022-10-06 01:10:12 -0400

    .

--- src/components/logOut.jsx
+++ src/components/logOut.jsx
@@ -5,8 +5,8 @@ import { auth } from "./firebase";
 export default function LogOut() {
   signOut(auth);
   return (
-    <>
+    <div className='d-flex justify-content-center allign-items-center'>
       <h1> See ya</h1>
-    </>
+    </div>
   );
 }
