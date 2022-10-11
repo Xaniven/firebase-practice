5877e6be86bd599c0939886430a3aec7cdc54edb
Xaniven <k.kelley5978@gmail.com>
2022-10-10 23:28:14 -0400

    added sccs for account page some styles to account page

--- src/components/account.jsx
+++ src/components/account.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "./firebase";
+import "./account.scss";
 
 export default function Account() {
   const [user, setuser] = useAuthState(auth);
 
   return (
-    <div>
-      <h1>{user.uid}</h1>
-      <p>{user.displayName}</p>
+    <div className='accountWrap'>
+      <h1>Welcome User: {user.uid}</h1>
+      <p>Email: {user.email}</p>
     </div>
   );
 }
