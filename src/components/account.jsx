d98a674eb2c95644e637dbd2efd37c41c14e573b
Xaniven <114899530+Xaniven@users.noreply.github.com>
2022-10-20 02:59:05 -0400

    Revert "fix: auth routing and firebase issues"

--- src/components/account.jsx
+++ src/components/account.jsx
@@ -1,22 +1,19 @@
-import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useNavigate } from 'react-router-dom';
-import './account.scss';
-import { auth } from './firebase';
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase";
+import "./account.scss";
+import { Navigate } from "react-router-dom";
 
 // Account pages
 export default function Account() {
-  const [user] = useAuthState(auth);
+  const [user, setuser] = useAuthState(auth);
 
-  const navigate = useNavigate();
-
-  if (!user) {
-    navigate('/Login');
-  }
-  return (
-    <div className="accountWrap container-fluid">
-      <h1>Welcome User: {user?.uid}</h1>
-      <p>Email: {user?.email}</p>
-    </div>
-  );
+  if (user != null)
+    return (
+      <div className='accountWrap container-fluid'>
+        <h1>Welcome User: {user.uid}</h1>
+        <p>Email: {user.email}</p>
+      </div>
+    );
+  if (user == null) return <Navigate to='/Login' />;
 }
--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,24 +1,24 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import 'firebase/auth';
-import { getAuth } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAvNqJNgTYQpeACzwPHLcDKW33ae1K-dJE',
-  authDomain: 'auth-test-681b0.firebaseapp.com',
-  projectId: 'auth-test-681b0',
-  storageBucket: 'auth-test-681b0.appspot.com',
-  messagingSenderId: '448055177583',
-  appId: '1:448055177583:web:39b50bca0d2b9d591bb89a',
+  apiKey: "AIzaSyAvNqJNgTYQpeACzwPHLcDKW33ae1K-dJE",
+  authDomain: "auth-test-681b0.firebaseapp.com",
+  projectId: "auth-test-681b0",
+  storageBucket: "auth-test-681b0.appspot.com",
+  messagingSenderId: "448055177583",
+  appId: "1:448055177583:web:39b50bca0d2b9d591bb89a",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+export const auth = getAuth();
 
 // export async function login(email, password) {
 //   await signInWithEmailAndPassword(auth, email, password)
