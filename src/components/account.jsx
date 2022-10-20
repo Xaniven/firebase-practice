70d28eb02d0006cfff2d193cc6224c9d10fccd45
Xaniven <k.kelley5978@gmail.com>
2022-10-20 02:02:14 -0400

    work on account page

--- src/components/account.jsx
+++ src/components/account.jsx
@@ -1,19 +1,70 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "./firebase";
 import "./account.scss";
 import { Navigate } from "react-router-dom";
+import { Button, Form } from "react-bootstrap";
+import { updateProfile } from "firebase/auth";
 
 // Account pages
-export default function Account() {
-  const [user, setuser] = useAuthState(auth);
 
-  if (user != null)
+export default function Account() {
+  async function update(auth, userRef) {
+    updateProfile(auth.currentUser, {
+      displayName: userRef.current.value,
+    })
+      .preventDefault.then(() => {
+        // ...
+      })
+      .catch((error) => {
+        // An error occurred
+        // ...
+      });
+  }
+
+  const userRef = useRef();
+  const [user] = useAuthState(auth);
+
+  if (user !== null) {
+    user.providerData.forEach((profile) => {
+      console.log("Sign-in provider: " + profile.providerId);
+      console.log("  Provider-specific UID: " + profile.uid);
+      console.log("  Name: " + profile.displayName);
+      console.log("  Email: " + profile.email);
+      console.log("  Photo URL: " + profile.photoURL);
+    });
+
     return (
       <div className='accountWrap container-fluid'>
-        <h1>Welcome User: {user.uid}</h1>
-        <p>Email: {user.email}</p>
+        <Form>
+          <Form.Group
+            className=' mb-3'
+            controlId='userNameChange'
+            onSubmit={() => update(auth.currentUser, userRef.current.value)}
+          >
+            <Form.Label>
+              Current Username:{" "}
+              {user.displayName == null ? "No username set" : user.providerId.displayName}
+            </Form.Label>
+            <Form.Control ref={userRef} type='text' placeholder='Enter new username' required />
+            <Form.Text className='text-muted'>Enter a new username</Form.Text>
+            <Button type='submit' className='m-2'>
+              Save
+            </Button>
+          </Form.Group>
+        </Form>
       </div>
     );
-  if (user == null) return <Navigate to='/Login' />;
+  }
+  if (user === null) return <Navigate to='/Login' />;
 }
+
+// if (user != null)
+// return (
+//   <div className='accountWrap container-fluid'>
+//     <h1>Welcome User: {user.uid}</h1>
+//     <p>Email: {user.email}</p>
+//   </div>
+// );
+// if (user == null) return <Navigate to='/Login' />;
+// }
--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import "firebase/auth";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,14 +21,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 
-// export async function login(email, password) {
-//   await signInWithEmailAndPassword(auth, email, password)
-//     .then((userCredential) => {
-//       // Signed in
-//       // ...
-//     })
-//     .catch((error) => {
-//       const errorCode = error.code;
-//       const errorMessage = error.message;
-//     });
-// }
+// Initialize Firestore
+const db = getFirestore(app);
