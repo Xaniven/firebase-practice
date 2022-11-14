270ddbd06a816d311fd88c7b06a92944617d5548
Xaniven <k.kelley5978@gmail.com>
2022-11-14 10:19:03 -0500

    Framer motion more comments throught project

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -4,9 +4,6 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAvNqJNgTYQpeACzwPHLcDKW33ae1K-dJE",
